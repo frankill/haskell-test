fa1035b92e2c635eaad9a38fc524b3f6c8b2aa23
frankill <34159384+frankill@users.noreply.github.com>
2020-03-03 23:44:58 +0800

    Create RecursiveContents.hs

---
+++ RecursiveContents.hs
@@ -0,0 +1,22 @@
+module RecursiveContents  where
+
+import Control.Monad (forM)
+import System.Directory (doesDirectoryExist, getDirectoryContents)
+import System.FilePath ((</>))
+
+simpleFind :: (FilePath -> Bool) -> FilePath -> IO [FilePath]
+simpleFind p path = do
+        names <- getRecursiveContents path
+        return (filter p names)
+
+getRecursiveContents :: FilePath -> IO [FilePath]
+getRecursiveContents topdir = do
+  names <- getDirectoryContents topdir
+  let properNames = filter (`notElem` [".", ".."]) names
+  paths <- forM properNames $ \name -> do
+                let path = topdir </> name
+                isDirectory <- doesDirectoryExist path
+                if isDirectory
+                then getRecursiveContents path
+                else return [path]
+  return (concat paths)
