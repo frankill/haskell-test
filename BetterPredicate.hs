a7b4501b01036b3b9f9182a5620ebe565c09afe6
frankill <34159384+frankill@users.noreply.github.com>
2020-03-04 20:36:06 +0800

    Update BetterPredicate.hs

--- BetterPredicate.hs
+++ BetterPredicate.hs
@@ -2,68 +2,33 @@ module BetterPredicate  where
 
 import Control.Monad (filterM)
 import System.Directory (Permissions(..), getModificationTime, getPermissions,searchable)
--- import System.Time (ClockTime(..))
-import System.FilePath (takeExtension)
+import System.Time (ClockTime(..))
 import Control.Exception (bracket, handle,SomeException)
 import System.IO (IOMode(..), hClose, hFileSize, openFile)
-
--- the function we wrote earlier
 import RecursiveContents (getRecursiveContents)
 
-type InfoP a =  FilePath        -- path to directory entry
-            -> Permissions     -- permissions
-            -> Maybe Integer   -- file size (Nothing if not file)
-            -- -> ClockTime       -- last modified
-            -> a
+data Info = Info {
+      infoPath :: FilePath
+    , infoPerms :: Maybe Permissions
+    , infoSize :: Maybe Integer
+    , infoModTime :: Maybe ClockTime
+    } deriving (Eq, Ord, Show)
 
-pathP :: InfoP FilePath
-pathP path _ _  = path
+liftP :: (a -> b -> c) -> (Info -> a) -> a -> Info  -> Bool
+liftP q f k d  = f d `q` k
 
-permP :: InfoP Bool
-permP _ perms _ = searchable perms
+(==?), (<?), (>?) :: (Ord a) => (Info -> a) -> a -> Info -> Bool
+(>?) = liftP (>)
+(<?) = liftP (<)
+(==?) = liftP (==)
 
-sizeP :: InfoP Integer
-sizeP _ _ (Just size)  = size
-sizeP _ _ Nothing      = -1
+liftP2 :: (a -> b -> c) -> (Info -> a) -> (Info -> b) -> Info -> Bool
+liftP2 q f g d = f d `q` g d
 
--- equalP :: (Eq a) => InfoP a -> a -> InfoP Bool
--- equalP f k = \w x y  -> f w x y  == k
-
--- equalP :: (Eq a) => InfoP a -> a -> InfoP Bool
--- equalP f k w x y = f w x y == k
-
-liftP :: (a -> b -> c) -> InfoP a -> b -> InfoP c
-liftP q f k w x y  = f w x y `q` k
-
-equalP, greaterP, lesserP :: (Ord a) => InfoP a -> a -> InfoP Bool
-greaterP = liftP (>)
-lesserP = liftP (<)
-equalP = liftP (==)
-
-liftP2 :: (a -> b -> c) -> InfoP a -> InfoP b -> InfoP c
-liftP2 q f g w x y  = f w x y  `q` g w x y 
-
-andP,orP :: InfoP a -> InfoP b -> InfoP Bool
-andP = liftP2 (&&)
-orP = liftP2 (||)
-
--- andP (sizeP `equalP` 1024) (pathP `equalP` "abc") 
-
--- liftPath :: (FilePath -> a) -> InfoP a
--- liftPath f w _ _ _ = f w
-
--- myTest2 = (liftPath takeExtension `equalP` ".cpp") `andP`
---          (sizeP `greaterP` 131072)
-
-(==?) = equalP
-(&&?) = andP
-(>?) = greaterP
-(<?) = lesserP
-(||?) = orP
-
- -- (sizeP >? 1024) ||? (pathP ==? "abc") 
---myTest3 = (liftPath takeExtension ==? ".cpp") &&? (sizeP >? 131072)
-
+(&&?),(||?) :: (Info -> a) -> (Info -> b) -> Info -> Bool
+(&&?) = liftP2 (&&)
+(||?) = liftP2 (||)
+ 
 err ::  SomeException -> IO (Maybe Integer)
 err _ =  return Nothing 
 
@@ -72,10 +37,11 @@ getFileSize path = handle err $ do
     bracket (openFile path ReadMode) hClose $ \h ->  
         hFileSize h >>= (\x -> return (Just x))  
 
-betterFind :: InfoP Bool -> FilePath -> IO [FilePath]
-betterFind p path = getRecursiveContents path >>= filterM check
-   where check name = do
-           perms <- getPermissions name
-           size <- getFileSize name
-           -- modified <- getModificationTime name
-           return (p name perms size)
+betterFind :: (Info -> Bool ) -> FilePath -> IO [FilePath]
+betterFind p path = getRecursiveContents path >>= filterM check 
+    where 
+		check name = do
+				   perms <- getPermissions name
+				   size <- getFileSize name
+				   modified <- getModificationTime name
+				   return (p $ Info name (Just perms) size (Just modified))
